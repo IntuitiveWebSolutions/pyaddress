0517ef1b2690599f9f312200e87854a6e5d5353d
Gabe D <me@gabe.io>
2020-01-28 20:33:36 -0500

    adds missed reference

--- address/dstk.py
+++ address/dstk.py
@@ -214,7 +214,7 @@ def post_multipart(host, selector, fields, files):
     Return the server's response page.
     """
     content_type, body = encode_multipart_formdata(fields, files)
-    h = httplib.HTTP(host)
+    h = http_client.HTTP(host)
     h.putrequest('POST', selector)
     h.putheader('content-type', content_type)
     h.putheader('content-length', str(len(body)))
